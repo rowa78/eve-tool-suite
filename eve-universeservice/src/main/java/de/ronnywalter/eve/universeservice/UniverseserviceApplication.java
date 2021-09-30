31d24857e239cda4995e7cc3478faab09e5d4f9c
Ronny Walter <mail@ronnywalter.de>
2021-09-30 06:49:08 +0200

    .

---
+++ eve-universeservice/src/main/java/de/ronnywalter/eve/universeservice/UniverseserviceApplication.java
@@ -0,0 +1,13 @@
+package de.ronnywalter.eve.universeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverseserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniverseserviceApplication.class, args);
+	}
+
+}
